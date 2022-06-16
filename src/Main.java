ba7f7e517352d7868fe153b7bb20cd02cef56535
Artem <artemastafev99@gmail.com>
2022-06-16 18:08:33 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class  Main {
+    public static void main(String[] args) {
+        Payer payer1 = new Payer("dog","doger", "1234", "4321");
+        System.out.println(Payer1.toString());
+    }
+
+}
